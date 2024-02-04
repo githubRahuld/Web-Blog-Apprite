f891c2746dc5823992744993b46f6347ed94f0ee
Rahul Dhakad <rahuldhakad201.rd@gmail.com>
2024-02-04 12:55:29 +0530

    add components ,store and pages

---
+++ src/components/Logo.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import logo from "../../public/images/logo.png";
+function Logo({ width = "1px" }) {
+  return <img src={logo} width={"55px"} />;
+}
+
+export default Logo;
